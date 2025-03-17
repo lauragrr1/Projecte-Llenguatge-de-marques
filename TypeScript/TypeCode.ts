00317f503856128106cb02889c5cc91e8f6ec10c
lauragrr1 <lalapato2000@gmail.com>
2025-03-18 00:49:16 +0100

    typecode

--- TypeScript/TypeCode.ts
+++ TypeScript/TypeCode.ts
@@ -1,10 +1,21 @@
+// Interficie per als festius 
 interface Festiu {
-    Date: string; // Data del festiu en format YYYY-MM-DD
-    LocalName: string; // Nom local del festiu
-    Name: string; // Festiu
-    National: boolean; // Indica si es un festiu nacional
-    SubdivisionCodes: string[]; // per exemple, provincies
+  Date: string; // Data del festiu en format YYYY-MM-DD
+  LocalName: string; // Nom local del festiu
+  Name: string; // Nom del festiu
+  National: boolean; // Indica si es un festiu nacional
+  SubdivisionCodes: string[]; // per exemple, provincies
   }
   
-  type FestiuArray = Festiu[]; // Array d'objectes tipus Festiu
-  
+// Interficie per a les tasques
+interface Tasca {
+  Date: string; // Data de la tasca en format YYYY-MM-DD
+  Title: string; // Títol de la tasca
+  Description: string; // Descripció de la tasca
+  Hour: string; // Hora de la tasca
+  Status: boolean; // Estat de la tasca (completada o no )
+}
+
+// Tipus per a arrays de festius i tasques
+type FestiuArray = Festiu[]; 
+type TascaArray = Tasca[]; 
