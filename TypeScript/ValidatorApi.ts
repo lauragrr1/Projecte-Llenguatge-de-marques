0c10ceec6d4c759a29501efbd93c900c55a0008b
Laura <klgrageda05@gmail.com>
2025-03-18 09:01:09 +0100

    Guardando cambios antes del pull

--- TypeScript/ValidatorApi.ts
+++ TypeScript/ValidatorApi.ts
@@ -1,14 +1,14 @@
-import Ajv from "ajv";
-import {Festiu, Tasca, FestiuArray, TascaArray } from "./TypeCode"; 
-import festiusSchema from "./SchemaFestius.json"; 
-import tasquesSchema from "./SchemaTasques.json";
+import Ajv from "Ajv";
+import {Festiu, Tasca, FestiuArray, TascaArray } from "TypeCode.ts"; 
+import festiusSchema from "../Json/SchemaFestius.json"; 
+import tasquesSchema from "../Json/schemaTasques.json";
 
 const fs = require('fs'); // Importem el mòdul fs per llegir fitxers locals
 const path = require('path'); // Importem path per gestionar rutes de fitxers
 
 // Llegeix els fitxers JSON locals
 const festiusData = fs.readFileSync('path/to/csvjson.json', 'utf-8');
-const festius: FestiuArray = JSON.parse(festiuData);
+const festius: FestiuArray = JSON.parse(festiusData);
 const tasquesData = fs.readFileSync('path/to/tasques.json', 'utf-8');
 const tasques: TascaArray = JSON.parse(tasquesData);
 
@@ -19,7 +19,7 @@ const validateTasques = ajv.compile(tasquesSchema);
 
 // Valida els festius
 festius.forEach(element => {
-    if (!validateFestius(festiu)) {
+    if (!validateFestius(Festiu)) {
         console.log("Error de validació en festiu: ", validateFestius.errors);
     }
 });
